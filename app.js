91b406e7e5c3725e796c77fb6766446d7a8a20aa
mistastone <michael.stone2@gmail.com>
2014-08-08 21:24:10 -0400

    Movies in specific genre displayed on page

--- app.js
+++ app.js
@@ -6,32 +6,38 @@ var movieApp = {};
 movieApp.key = "7fb7426481eed21217e4ee7dc69256d4";
 
 movieApp.init = function() {
-	movieApp.getGenre('drama');
+
+	// CODE FOR DROPDOWN MENU
+	movieApp.getGenre('18');
+	// change to .click
 	$('#genre').on('change', function(){
 		var genre = $(this).val();
+		// var rating
+		// var length
 		console.log(genre);
+		movieApp.getGenre(genre);
 	});
 }
 
 movieApp.getGenre = function(query){
 	$.ajax ({
-		url: 'https://api.themoviedb.org/3/genre/movie/list',
+		url: 'https://api.themoviedb.org/3/genre/'+ query +'/movies',
 		type: 'GET',
 		data: {
-			key: movieApp.key,
-			format: 'jsonp',
-			q: query
+			api_key: movieApp.key
+			// format: 'jsonp'
 		},
 		dataType: 'jsonp',
 		success: function(result){
-			// console.log(result)
-			movieApp.displayGenre(result);
+			console.log(result);
+			movieApp.displayMovie(result.results);
 		}
 	});
 }
 
 movieApp.displayMovie = function(data){
 	$.each(data, function(i, movie){
+		console.log(movie.title);
 		var title = $('<h2>').text(movie.title);
 		var thisGenre = $('<div>').addClass('movie').append(title);
 		$('#genreType').append(thisGenre);
@@ -41,3 +47,6 @@ movieApp.displayMovie = function(data){
 $(function(){
 	movieApp.init();
 });
+
+// Getgenre automatically runs when the drop down is selected
+// Don't run on 'change' run '.clicj'
