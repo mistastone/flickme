e682954c2400b0131a2dac167c6a5d6f5d80b568
mistastone <michael.stone2@gmail.com>
2014-08-10 12:16:59 -0400

    Genre/Year/(specific) Score all working. Also included a minimum vote count of 50 to filter out weird ass Japanese porno

--- app.js
+++ app.js
@@ -12,36 +12,40 @@ movieApp.init = function() {
 	$('#clickMe').on('click', function(){
 		var genre = $('#genre').val();
 		var year = $('#searchYear').val();
+		var score = $('#searchScore').val();
 		// var rating
 		// var length
 		console.log(year);
 		console.log(genre);
+		console.log(score);
 		$('#genreType').empty();
-		movieApp.getGenre(genre);
+		// movieApp.getGenre(genre);
 		movieApp.getYears(year);
 	});
-
 }
 
-movieApp.getGenre = function(query){
-	$.ajax ({
-		url: 'https://api.themoviedb.org/3/genre/'+ query +'/movies',
-		type: 'GET',
-		data: {
-			api_key: movieApp.key
-			// format: 'jsonp'
-		},
-		dataType: 'jsonp',
-		success: function(result){
-			console.log(result);
-			movieApp.displayMovie(result.results);
-		}
-	});
-}
+// movieApp.getGenre = function(query){
+// 	$.ajax ({
+// 		url: 'https://api.themoviedb.org/3/genre/'+ query +'/movies',
+// 		type: 'GET',
+// 		data: {
+// 			api_key: movieApp.key
+// 			// format: 'jsonp'
+// 		},
+// 		dataType: 'jsonp',
+// 		success: function(result){
+// 			console.log(result);
+// 			movieApp.displayMovie(result.results);
+// 		}
+// 	});
+// }
 
 movieApp.getYears = function(query){
+	var genre = $('#genre').val();
+	var score = $('#searchScore').val();
 	$.ajax ({
-		url: 'https://api.themoviedb.org/3/discover/movie?year='+ query,
+		url: 'https://api.themoviedb.org/3/discover/movie?vote_average='+ score +'&primary_release_year='+ query + '&api_key='+ movieApp.key +'&vote_count.gte=50&with_genres=' + genre,
+		// url: 'https://api.themoviedb.org/3/discover/movie?year='+ query ,
 		type: 'GET',
 		data: {
 			api_key: movieApp.key
@@ -58,10 +62,9 @@ movieApp.getYears = function(query){
 movieApp.displayMovie = function(data){
 	$.each(data, function(i, movie){
 		console.log(movie.title);
-		console.log(movie.release_date);
 		var title = $('<h2>').text(movie.title);
 		var poster = $('<img>').attr('src', 'http://image.tmdb.org/t/p/w396/' + movie.poster_path);
-		var thisYear = $('<p>').text(movie.year);
+		var thisYear = $('<p>').text(movie.primary_release_year);
 		var thisGenre = $('<div>').addClass('movie').append(title, poster, thisYear);
 		$('#genreType').append(thisGenre, thisYear);
 	});
@@ -72,4 +75,4 @@ $(function(){
 });
 
 // Get genre automatically runs when the drop down is selected
-// Don't run on 'change' run '.clicj'
+// Don't run on 'change' run '.click'
